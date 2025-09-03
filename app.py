148f63b09d5ef4ecd3fa06cfa50ff6d0fb5a0df3
heamgadiyaram <heam.gadiyaram07@gmail.com>
2025-09-03 17:44:58 -0400

    setup database and added create, read, and delete operations

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from routes import travels_bp
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(travels_bp, url_prefix="/api")
+
+    @app.get("/")
+    def root():
+        return {"message": "Travel Journal API"}, 200
+
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
